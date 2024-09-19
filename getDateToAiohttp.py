31678d2d0c530caffe2aa48f047cc50e9dba6384
xoiox <xoioxser@gmail.com>
2024-09-19 15:06:54 +0300

    изменения в функции получения данных

--- getDateToAiohttp.py
+++ getDateToAiohttp.py
@@ -9,7 +9,11 @@ nameFileToSite = "site.json"
 siteData =None
 with open(nameFileToSite, "r", encoding="utf-8") as f:
     siteData =  json.load(f)["webSite"]
-siteData = [{**sd,"status":None, "dateLastCheck":None,"textError":None} for sd in siteData]
+n=1
+siteData = [{**sd,"status":None, "dateLastCheck":None,"textError":None, "NPP":0,"statusCode":None} for sd in siteData]
+for sd in siteData:
+    sd["NPP"]=str(n)
+    n=n+1
 
 counter = 0
 lock = asyncio.Lock()
@@ -18,35 +22,36 @@ lock = asyncio.Lock()
 async def fetch(elm,session):
     global counter
     try:
-        resp = session.get(elm["url"], timeout=5)
+        resp = await session.get(elm["url"], timeout=5)
         elm["status"] = "Успех"
+        statusCode = resp.status
         elm["dateLastCheck"] = datetime.datetime.now()
+        elm["statusCode"] =statusCode
         async with lock:
             counter += 1
-            print(counter)
-    except asyncio.TimeoutException:
+            print(str(counter) +" "+ elm["NPP"])
+    except asyncio.TimeoutError:
         elm["status"] = "Ошибка"
         elm["textError"] = "Ошибка по таймауту"
         elm["dateLastCheck"] = datetime.datetime.now()
         async with lock:
             counter += 1
-            print(counter)
-    except aiohttp.RequestError as err:
+            print(str(counter) +" "+ elm["NPP"])
+    except aiohttp.ClientError  as err:
         elm["status"] = "Ошибка клиента"
         elm["textError"] = str(err)
         elm["dateLastCheck"] = datetime.datetime.now()
         async with lock:
             counter += 1
-            print(counter)
-    except Exception as err:
-        print(err)
+            print(str(counter) +" "+ elm["NPP"])
+
 
 
 
 
 #создаем массив задач и сессию
 async def fetchAll(siteData):
-        ses=  aiohttp.client()
+    async with aiohttp.ClientSession() as ses:
         tasks = [fetch(elm,ses) for elm in siteData]
         return await asyncio.gather(*tasks)
 
@@ -56,7 +61,11 @@ async def mainLoop():
     res = await fetchAll(siteData)
 
 
-asyncio.run(mainLoop())
+while(True):
+    asyncio.run(mainLoop())
+    comand = input()
+    if comand=="exit":
+        break
 
-print(*siteData, sep="\n")
+#print(*siteData, sep="\n")
 print("End")
